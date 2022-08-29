c16636f90ba5746d66a0dcfad486a7c0e461ce77
anjujd <107190673+anjujd@users.noreply.github.com>
2022-08-29 17:42:30 +0530

    commit1

--- main.tf
+++ main.tf
@@ -1,18 +1,39 @@
-# Configure the AWS Provider
-provider "aws" {
-  region     = "us-west-2"
- }
+terraform {
+  cloud {
+    hostname = "terraform-enterprise.eastus.cloudapp.azure.com"
+    organization = "EY"
 
-resource "aws_s3_bucket" "c1234bucketanju" {
-  bucket = "c1234bucketanju"
-  tags = {
-    Name        = "Mys3buckets"
-    Environment = "Testcase"
+    workspaces {
+      name = "Anju-test-workspace"
+    }
+  }
+    required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.16.0"
+    }
   }
 }
-resource "aws_s3_bucket_public_access_block" "c1234bucketanju" {
-  bucket = aws_s3_bucket.c1234bucketanju.id
-  block_public_acls   = false
 
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_automation_account" "example" {
+  name                = "example-account"
+  location            = "West Europe"
+  resource_group_name = "example-resources"
+  sku_name            = "Basic"
+
+  tags = {
+  cost_center = "__COST_CENTER__"
+  ppmc_id     = "__PPMC_ID__"
+  toc         = "__TOC__"
+  usage_id    = "__USAGE_ID__"
+  env_type    = "_ENV_TYPE__"
+  exp_date    = "__EXP_DATE__"
+  endpoint    = "__ENDPOINT__"
+  sd_period   = "___SD_PERIOD__"
+  }
 }
 
